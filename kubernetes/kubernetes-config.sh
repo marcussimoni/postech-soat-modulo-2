bf1ccbaa2757b7e779aa8c450698214a916bd52e
marcus <marcussimoni@gmail.com>
2023-08-11 19:35:19 -0300

    ajustes nas configurações do kubernetes

---
+++ kubernetes/kubernetes-config.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+declare -a DIRECTORIES
+VALID_PARAM=0
+if [ "$1" = "apply" ]
+then
+  VALID_PARAM=1
+  DIRECTORIES+=("configMap")
+  DIRECTORIES+=("secrets")
+  DIRECTORIES+=("postgres")
+  DIRECTORIES+=("api")
+fi
+
+if [ "$1" = "delete" ]
+then
+  VALID_PARAM=1
+  DIRECTORIES+=("api")
+  DIRECTORIES+=("postgres")
+  DIRECTORIES+=("secrets")
+  DIRECTORIES+=("configMap")
+fi
+
+if [ $VALID_PARAM -eq 1 ]
+then
+  for d in ${DIRECTORIES[@]}
+    do
+      echo "executando: kubectl $1 -f ./kubernetes/$d"
+      kubectl $1 -f ./kubernetes/$d
+    done
+fi
